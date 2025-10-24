f90fa65d19452fceac080edc23842e3f804c7c03
Kalimi Sultan <developersultan100@gmail.com>
2025-10-24 11:58:18 +0600

    Class01/data.js added

---
+++ CommonJS Module/data.js
@@ -0,0 +1,48 @@
+//Named Exports
+
+exports.dev = [
+  {
+    name: "Kalimi Sultan",
+    joinDate: 2022,
+    skill: "Frontend",
+  },
+  {
+    name: "Hasib Hasan",
+    joinDate: 2020,
+    skill: "Laravel",
+  },
+  {
+    name: "Ahmed Sojib",
+    joinDate: 2024,
+    skill: "WordPress",
+  },
+  {
+    name: "Mohammad Ali",
+    joinDate: 2018,
+    skill: "Full Stack",
+  },
+];
+
+exports.copules = [
+  {
+    name: "Kalimi",
+    gender: "Male",
+    year: 2001,
+  },
+  {
+    name: "Jesmin",
+    gender: "Female",
+    year: 2001,
+  },
+  {
+    name: "Hasib",
+    gender: "Male",
+    year: 2010,
+  },
+];
+
+exports.student = {
+  name: "Rafiq",
+  session: "2021",
+  subject: "CSE",
+};
---
+++ CommonJS Module/function.js
@@ -0,0 +1,1 @@
+const ageCal = (name, year) => {};
